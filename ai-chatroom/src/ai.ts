42c07e0701de8fcff68c3be238e74b57830a3caf
Aditya Patange <contact.adityapatange@gmail.com>
2025-01-02 12:47:52 +0530

    ai: refactor

--- ai-chatroom/src/ai.ts
+++ ai-chatroom/src/ai.ts
@@ -55,11 +55,18 @@ const anthropicModels = [
   "claude-3-haiku-20240307",
 ];
 
-const modelsByProvider: {
-  [provider: string]: Array<string>;
-} = {
-  anthropic: anthropicModels,
-  openai: openAIModels,
+interface ModelProvider {
+  availableModels: Array<string>;
+  initializer: (modelId: string) => LanguageModel;
+}
+
+interface ModelsByProvider {
+  [provider: string]: ModelProvider;
+}
+
+const modelsByProvider: ModelsByProvider = {
+  anthropic: { availableModels: anthropicModels, initializer: anthropic },
+  openai: { availableModels: openAIModels, initializer: openai },
 };
 
 interface GenerateTextOptions {
@@ -102,11 +109,21 @@ export class AI {
   }
 
   /**
+   * Initializes an instance of the AI class with the specified model.
+   * @param modelName The name of the model to instantiate the AI client with.
+   * @returns AI instance
+   */
+  public static async getInstance(modelName: string): Promise<AI> {
+    const model = await AI.getModel(modelName);
+    return new AI(model);
+  }
+
+  /**
    * Generates text using the specified options.
    * @param options - The options for generating text.
    * @returns The generated text.
    */
-  async generateText(
+  public async generateText(
     options: GenerateTextOptions
   ): Promise<ReturnType<typeof generateText>> {
     return await generateText({ ...options, model: this.model });
@@ -117,7 +134,7 @@ export class AI {
    * @param options - The options for generating an object.
    * @returns The generated object.
    */
-  async generateObject(
+  public async generateObject(
     options: GenerateObjectOptions
   ): Promise<ReturnType<typeof generateObject>> {
     return await generateObject({ ...options, model: this.model });
@@ -129,23 +146,36 @@ export class AI {
    * @returns The language model.
    * @throws An error if the model is not found.
    */
-  static async getModel(modelName: string): Promise<LanguageModel> {
+  public static async getModel(modelName: string): Promise<LanguageModel> {
     try {
-      const [provider, model] = modelName.split(":");
+      const { model, provider } =
+        AI.extractModelAndProviderFromModelConfig(modelName);
 
-      if (AI.providerHasModel(provider, model)) {
-        if (provider == "anthropic") {
-          return anthropic(model);
-        } else if (provider == "openai") {
-          return openai(model);
-        }
+      if (!AI.providerHasModel(provider, model)) {
+        throw new Error(`AI: model ${modelName} not found`);
       }
 
-      throw new Error(`AI: provider ${provider} not found`);
+      return AI.initializeModelByProvider(provider, model);
     } catch (error) {
       console.error(error);
       throw new Error(`AI: model ${modelName} not found`);
     }
+  }
+
+  /**
+   * Extracts the model and provider from the composite model name.
+   * @param modelName The composite model name.
+   * @returns The 'model' and 'provider' extracted from the composite model name.
+   */
+  private static extractModelAndProviderFromModelConfig(modelName: string): {
+    model: string;
+    provider: string;
+  } {
+    const [provider, model] = modelName.split(":");
+    return {
+      provider,
+      model,
+    };
   }
 
   /**
@@ -155,16 +185,21 @@ export class AI {
    * @returns true if the provider has the model.
    */
   private static providerHasModel(provider: string, model: string): boolean {
-    return Boolean(modelsByProvider?.[provider]?.includes(model));
+    return Boolean(
+      modelsByProvider?.[provider]?.availableModels?.includes(model)
+    );
   }
 
   /**
-   * Initializes an instance of the AI class with the specified model.
-   * @param modelName The name of the model to instantiate the AI client with.
-   * @returns AI instance
+   * Initializes the model by the provider.
+   * @param provider The LLM provider.
+   * @param model The LLM model.
+   * @returns The initialized model.
    */
-  public static async getInstance(modelName: string): Promise<AI> {
-    const model = await AI.getModel(modelName);
-    return new AI(model);
+  private static initializeModelByProvider(
+    provider: string,
+    model: string
+  ): LanguageModel {
+    return modelsByProvider[provider].initializer(model);
   }
 }
